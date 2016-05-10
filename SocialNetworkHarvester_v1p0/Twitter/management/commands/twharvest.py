195cad8cad12551103b6f6b2d6504484dbe13261
Sam <s.c_123@hotmail.com>
2016-05-10 01:36:27 -0400

    Little changes

--- SocialNetworkHarvester_v1p0/Twitter/management/commands/twharvest.py
+++ SocialNetworkHarvester_v1p0/Twitter/management/commands/twharvest.py
@@ -3,13 +3,15 @@ from django.core.management.base import BaseCommand
 from tendo import singleton
 from .harvest import harvestTwitter
 from SocialNetworkHarvester_v1p0.settings import twitterLogger
-
+import datetime
+now = datetime.datetime.now()
 
 class Command(BaseCommand):
     help = 'Search the Twitter\'s API for new content'
 
     def handle(self, *args, **options):
         me = singleton.SingleInstance(flavor_id="crontw")
+        print('%s: Will run the Twitter harvester'%now.strftime('%y-%m-%d_%H:%M'))
         twitterLogger.log("Will run the Twitter harvesters.")
         try:
             harvestTwitter()
