ef00a2b9caeafd6d48ea65f293668fe04720c0e3
Sam <s.c_123@hotmail.com>
2016-05-13 17:03:34 -0400

    A mistake. corrected

--- SocialNetworkHarvester_v1p0/static/js/table_script.js
+++ SocialNetworkHarvester_v1p0/static/js/table_script.js
@@ -1,27 +1,33 @@
 $.getScript("/static/js/DataTables-1.10.9/js/jquery.dataTables.min.js")
 $.getScript("/static/js/Select-1.0.1/js/dataTables.select.min.js")
-$.getScript("/static/js/linkify/linkify.min.js")
-$.getScript("/static/js/linkify/linkify-string.min.js")
+$.getScript("/static/js/linkify/linkify.min.js", function () {
+    log("min loaded");
+    $.getScript("/static/js/linkify/linkify-string.min.js", function () {
+        log("string loaded")
+    })
+})
 
 
 var selectedTableRows = [];
 var maxSelecteditems = 1000;
-function maxSelectionCallback(){alert("You can only select "+maxSelecteditems+" items at a time!");}
+function maxSelectionCallback() {
+    alert("You can only select " + maxSelecteditems + " items at a time!");
+}
 
 var default_asSorting = ["desc", "asc", "none"];
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $(".section_title").click(function(){
+    $(".section_title").click(function () {
         var content = $(this).parent().next(".section_content");
         var options = $(this).next(".section_options");
         var table = content.children();
-        if(options.length != 0){
+        if (options.length != 0) {
             menuToggle(options);
         }
-        if(content.length != 0){
+        if (content.length != 0) {
             content.slideToggle(300);
-            if (table.attr('drawn') == 'False'){
+            if (table.attr('drawn') == 'False') {
                 drawTable(table);
             }
         }
@@ -29,42 +35,43 @@ $(document).ready(function() {
         togglePlusMinusSign($(this).parent().children(".tableOpenCloseIcon"))
     });
 
-    $('.tableOpenCloseIcon').click(function(){
+    $('.tableOpenCloseIcon').click(function () {
         var content = $(this).parent().next(".section_content");
         var options = $(this).prev(".section_options");
         var table = content.children();
-        if(options.length != 0){
+        if (options.length != 0) {
             menuToggle(options);
         }
-        if(content.length != 0){
+        if (content.length != 0) {
             content.slideToggle(300);
-            if (table.attr('drawn') == 'False'){
+            if (table.attr('drawn') == 'False') {
                 drawTable(table);
             }
         }
         togglePlusMinusSign($(this));
     });
 
-    $('.table_select_master').each(function(){
-        $(this).prop('checked', false) ;
-    }).click(function(){
+    $('.table_select_master').each(function () {
+        $(this).prop('checked', false);
+    }).click(function () {
         var table = $(this).parents('.display');
         setProcessing(table, true);
         var fullURL = table.DataTable().ajax.json()['fullURL']
         var modifiedURL = fullURL.replace(/iDisplayStart=[0-9]*/, 'iDisplayStart=0');
-        var modifiedURL = modifiedURL.replace(/iDisplayLength=[0-9]*/, 'iDisplayLength='+(maxSelecteditems+1));
-        if($(this).prop('checked')){
-            $.ajax({"url":modifiedURL,
-                "success": function(data){
-                    data.data.some(function(item){
-                        if (selectedTableRows.length < maxSelecteditems){
+        var modifiedURL = modifiedURL.replace(/iDisplayLength=[0-9]*/, 'iDisplayLength=' + (maxSelecteditems + 1));
+        if ($(this).prop('checked')) {
+            $.ajax({
+                "url": modifiedURL,
+                "success": function (data) {
+                    data.data.some(function (item) {
+                        if (selectedTableRows.length < maxSelecteditems) {
                             pushUniqueIn(selectedTableRows, item['DT_RowId']);
                         } else {
                             maxSelectionCallback()
                             return true;
                         }
                     });
-                    table.find('tr').each(function(){
+                    table.find('tr').each(function () {
                         $(this).addClass('selected');
                     });
                     setProcessing(table, false);
@@ -72,12 +79,13 @@ $(document).ready(function() {
                 }
             })
         } else {
-            $.ajax({"url":modifiedURL,
-                "success": function(data){
-                    data.data.forEach(function(item){
+            $.ajax({
+                "url": modifiedURL,
+                "success": function (data) {
+                    data.data.forEach(function (item) {
                         removeFrom(selectedTableRows, item['DT_RowId']);
                     });
-                    table.find('tr').each(function(){
+                    table.find('tr').each(function () {
                         $(this).removeClass('selected');
                     });
                     setProcessing(table, false);
@@ -87,95 +95,95 @@ $(document).ready(function() {
         }
     });
 
-    $('[id="reloadTableLink"]').click(function(){
+    $('[id="reloadTableLink"]').click(function () {
         var content = $(this).parent().parent().next(".section_content");
         var table = content.children().children("table");
         var scriptTag = table.children('.tableVars');
-        var dynamicSource=false;
-        var GETValues=null;
+        var dynamicSource = false;
+        var GETValues = null;
         eval(scriptTag.text())
-        var source = url+"?fields="+fields;
+        var source = url + "?fields=" + fields;
         if (dynamicSource) {
             source += getSourcesFromSelectedRows();
         }
-        if (GETValues != null){
+        if (GETValues != null) {
             source += obtainGETValues(GETValues);
         }
         table.DataTable().ajax.url(source);
         table.DataTable().ajax.reload();
     });
-    var snippetDelay=1500, tmOutFcn;
-    $("body").on('mouseover', '.snippetHover', function(event){
-        if($('#snippetContainer').length == 0) {
+    var snippetDelay = 1500, tmOutFcn;
+    $("body").on('mouseover', '.snippetHover', function (event) {
+        if ($('#snippetContainer').length == 0) {
             var href = $(this).attr('href') + "?snippet=true";
             var snippet = "<div id='snippetContainer'>" +
-                "<iframe id='snippet' scrolling='no' src="+href+"/>" +
+                "<iframe id='snippet' scrolling='no' src=" + href + "/>" +
                 "</div>"
             $("body").append(snippet);
             $('#snippetContainer').position({
                 my: "left+10 top",
                 of: event,
                 collision: "fit",
-                within:$("#content_container")
+                within: $("#content_container")
             })
             tmOutFcn = setTimeout(function () {
                 $('#snippet').css('display', 'block')
             }, snippetDelay);
         }
     });
-    $("body").on('mouseout', '.snippetHover',function(){
+    $("body").on('mouseout', '.snippetHover', function () {
         clearTimeout(tmOutFcn);
         $('#snippetContainer').remove();
     });
 
-    $(".option_checkbox").each(function(){
-        $(this).prop('checked', false) ;
-    }).click(function() {
+    $(".option_checkbox").each(function () {
+        $(this).prop('checked', false);
+    }).click(function () {
         var content = $(this).closest(".section_menu").next(".section_content");
         var table = content.children().children("table");
         var scriptTag = table.children('.tableVars');
-        var dynamicSource=false;
-        var GETValues=null;
+        var dynamicSource = false;
+        var GETValues = null;
         eval(scriptTag.text())
-        var source = url+"?fields="+fields;
+        var source = url + "?fields=" + fields;
         if (dynamicSource) {
             source += getSourcesFromSelectedRows();
         }
-        if (GETValues != null){
+        if (GETValues != null) {
             source += obtainGETValues(GETValues);
         }
-        if ($(this).prop('checked')){
-            source += "&"+$(this).attr('name')+"=true";
+        if ($(this).prop('checked')) {
+            source += "&" + $(this).attr('name') + "=true";
         }
         table.DataTable().ajax.url(source);
         table.DataTable().ajax.reload();
     });
 });
 
-function togglePlusMinusSign(sign){
+function togglePlusMinusSign(sign) {
     var src = sign.children('img').attr('src')
-    if(sign.attr('type') == 'plus') {
+    if (sign.attr('type') == 'plus') {
         src = src.replace(/\/[^\/]+\.png/, '/minus_icon_128.png')
         sign.attr('type', 'minus');
-    } else if (sign.attr('type') == 'minus'){
+    } else if (sign.attr('type') == 'minus') {
         src = src.replace(/\/[^\/]+\.png/, '/plus_icon_128.png')
         sign.attr('type', 'plus');
     }
     sign.children('img').attr('src', src)
 }
 
-function obtainGETValues(GETValues){
+function obtainGETValues(GETValues) {
     var ret = "";
-    GETValues.forEach(function(entry){
-        ret += "&"+entry;
+    GETValues.forEach(function (entry) {
+        ret += "&" + entry;
     });
     return ret;
 }
 
-function setProcessing(table, value){
+function setProcessing(table, value) {
     var oSettings = null;
-    table.dataTable().dataTableSettings.some(function(o){
-        if (o.nTable.id == $(table).attr('id')){
+    table.dataTable().dataTableSettings.some(function (o) {
+        if (o.nTable.id == $(table).attr('id')) {
             oSettings = o;
             return true;
         }
@@ -183,25 +191,25 @@ function setProcessing(table, value){
     table.dataTable().oApi._fnProcessingDisplay(oSettings, value);
 }
 
-function drawTable(table){
+function drawTable(table) {
     var language = {
         "processing": "Working on it...",
-        "thousands":",",
+        "thousands": ",",
     };
     var languageParams = {};
     var scriptTag = table.children('.tableVars');
-    var dynamicSource=false;
-    var GETValues=null;
+    var dynamicSource = false;
+    var GETValues = null;
     eval(scriptTag.text());
-    var source = url+"?fields="+fields;
+    var source = url + "?fields=" + fields;
     if (dynamicSource) {
         source += getSourcesFromSelectedRows();
     }
-    if (GETValues != null){
+    if (GETValues != null) {
         source += obtainGETValues(GETValues);
     }
-    if (languageParams){
-        for(var param in languageParams){
+    if (languageParams) {
+        for (var param in languageParams) {
             language[param] = languageParams[param];
         }
     }
@@ -215,8 +223,8 @@ function drawTable(table){
         "columnDefs": columnsDefs,
         "language": language,
         "processing": true,
-        "rowCallback": function( row, data ) {
-            if ( $.inArray(data.DT_RowId, selectedTableRows) !== -1 ) {
+        "rowCallback": function (row, data) {
+            if ($.inArray(data.DT_RowId, selectedTableRows) !== -1) {
                 $(row).addClass('selected');
             }
         }
@@ -226,24 +234,26 @@ function drawTable(table){
     table.attr('drawn', 'True');
 }
 
-function disableLiveInputSearch(){
+function disableLiveInputSearch() {
     $("div.dataTables_filter input").unbind()
-    .keyup( function (e) {
-        if (e.keyCode == 13) {
-            var table = $(this).parent().parent().parent().children('table')
-            table.dataTable().fnFilter(this.value);
-        }
-    });
+        .keyup(function (e) {
+            if (e.keyCode == 13) {
+                var table = $(this).parent().parent().parent().children('table')
+                table.dataTable().fnFilter(this.value);
+            }
+        });
 }
 
-function customSelectCheckbox(table){
+function customSelectCheckbox(table) {
     table.on('click', 'td.select-checkbox', function () {
         var id = $(this).parent().attr('id');
-        if (!$(this).parent().hasClass('selected')){
-            if (selectedTableRows.length < maxSelecteditems){
+        if (!$(this).parent().hasClass('selected')) {
+            if (selectedTableRows.length < maxSelecteditems) {
                 pushUniqueIn(selectedTableRows, id);
                 $(this).parent().addClass('selected');
-            } else {maxSelectionCallback();}
+            } else {
+                maxSelectionCallback();
+            }
         } else {
             removeFrom(selectedTableRows, id);
             $(this).parent().removeClass('selected');
@@ -252,60 +262,68 @@ function customSelectCheckbox(table){
     });
 }
 
-function pushUniqueIn(array, item){
+function pushUniqueIn(array, item) {
     var index = $.inArray(item, array);
     if (index === -1) {
         array.push(item);
     }
 }
 
-function removeFrom(array, item){
+function removeFrom(array, item) {
     var index = $.inArray(item, array);
     if (index != -1) {
         array.splice(index, 1);
     }
 }
 
-function toggleFrom(array, id){
+function toggleFrom(array, id) {
     var index = $.inArray(id, array);
     if (index === -1) {
         array.push(id);
     } else {
-        array.splice(index,1);
+        array.splice(index, 1);
     }
 }
 
-function menuToggle(elem){
-    if (elem.css('width') == '0px'){
+function menuToggle(elem) {
+    if (elem.css('width') == '0px') {
         elem.animate({
-            width: parseInt(elem.parent().css("width")) - parseInt(elem.prev(".section_title").css("width"))-35
-        },300);
+            width: parseInt(elem.parent().css("width")) - parseInt(elem.prev(".section_title").css("width")) - 35
+        }, 300);
     } else {
-        elem.animate({width:0},300);
+        elem.animate({width: 0}, 300);
     }
 }
 
-function getSourcesFromSelectedRows(){
+function getSourcesFromSelectedRows() {
     var sources = "";
-    for (var i = 0; i<selectedTableRows.length; i++){
-        sources += selectedTableRows[i]+",";
+    for (var i = 0; i < selectedTableRows.length; i++) {
+        sources += selectedTableRows[i] + ",";
     }
-    return "&selected_rows="+sources;
+    return "&selected_rows=" + sources;
 }
 
-function formatTweetText(text){
+function formatTweetText(text) {
+    text = linkifyStr(text, {linkClass: "TableToolLink"})
 
-    log(text)
-    var urlRegex = /(https?:\/\/t\.co\/([A-Z]|[0-9])+)/ig;
-    var url = urlRegex.exec(text);
-    text = linkifyStr(text)
-    //text = text.replace(urlRegex, '<a class="TableToolLink" target="_blank" href="' + url + '">' + url + '</a>');
-
-    var userRegex = /@([A-Z]|[0-9]|_)+/i;
-    var username = text.match(userRegex);
-    if (username != null) {
-        username = username[0];
-        text = text.replace(userRegex, '<a class="TableToolLink" target="_blank" href="/twitter/user/' + username.slice(1) + '">' + username + '</a>');
+    log(text);
+    var userRegex = /@([A-Z]|[0-9]|_)+/ig;
+    var usernames = text.match(userRegex);
+    if (usernames != null) {
+        usernames.forEach(function (username) {
+            log(username);
+            text = text.replace(username, '<a class="TableToolLink snippetHover" target="_blank" href="/twitter/user/' + username.slice(1) + '">' + username + '</a>');
+        });
     }
+
+    var hashtagRegex = /#([A-Z]|[0-9]|_)+/ig;
+    var hashtags = text.match(hashtagRegex);
+    if (hashtags != null) {
+        hashtags.forEach(function (hashtag) {
+            log(hashtag);
+            text = text.replace(hashtag, '<a class="TableToolLink" target="_blank" href="/twitter/hashtag/' + hashtag.slice(1) + '">' + hashtag + '</a>');
+        });
+    }
+
     return text;
 }
