615824ce99f6a926016eec85395a615b357ceeed
Sam <s.c_123@hotmail.com>
2016-10-09 16:22:37 -0400

    ignored wsgi.py and static folder

--- SocialNetworkHarvester_v1p0/SocialNetworkHarvester_v1p0/clean_wsgi.py
+++ SocialNetworkHarvester_v1p0/SocialNetworkHarvester_v1p0/clean_wsgi.py
@@ -1,6 +1,6 @@
 activate_this = '/PATH/TO/VIRTUAL/ENVIRONEMENT/bin/activate_this.py'
-with open(activate_this) as file:                                                                                       25
-exec(file.read(), dict(__file__=activate_this))
+with open(activate_this) as file:
+    exec(file.read(), dict(__file__=activate_this))
 
 import os
 import sys
