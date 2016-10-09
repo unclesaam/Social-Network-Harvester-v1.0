e9fe927c766bf544f1247ae4638d3ec1d6d37d6d
Sam <s.c_123@hotmail.com>
2016-10-09 16:11:59 -0400

    Created a clean version of wsgi.py, including virtualenv activation

---
+++ SocialNetworkHarvester_v1p0/SocialNetworkHarvester_v1p0/clean_wsgi.py
@@ -0,0 +1,15 @@
+activate_this = '/PATH/TO/VIRTUAL/ENVIRONEMENT/bin/activate_this.py'
+with open(activate_this) as file:                                                                                       25
+exec(file.read(), dict(__file__=activate_this))
+
+import os
+import sys
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+dname = os.path.join(dname,'..')
+sys.path.insert(0,dname)
+
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SocialNetworkHarvester_v1p0.settings")
+application = get_wsgi_application()  
